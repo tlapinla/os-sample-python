9b2402a9c0b1250d1aecdb48578820fee33a32f3
Petteri Lapinlampi <petteri.lapinlampi@ericsson.com>
2017-06-19 15:47:55 +0300

    Change greeting text

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello Bello World!"
 
 if __name__ == "__main__":
     application.run()
